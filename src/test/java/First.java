b931fd8efc5a491789fd84832d16a4ae565a992b
KristinaTomash <kristitomsh@gmail.com>
2025-02-24 20:43:57 +0200

    Commit1

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class First {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+
+        }
+
+    }
 }
